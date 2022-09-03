2b1d930eac7d853ea725f1f31c36aa6e5775eabf
pumegaming <pumepanarin@hotmail.com>
2022-09-04 00:13:22 +0700

    Oh god, I hope this fucking work--

---
+++ content/.vuepress/config.ts
@@ -0,0 +1,8 @@
+import { defineUserConfig } from 'vuepress-vite'
+
+export default defineUserConfig({
+  lang: 'en-US',
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+  base: '/Risus-RPG/'
+})
