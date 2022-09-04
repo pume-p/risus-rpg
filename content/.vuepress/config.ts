a7cee67f1698db5ac993003e07f8af0f538a53ca
pumegaming <pumepanarin@hotmail.com>
2022-09-04 16:32:20 +0700

    switch to hope theme

--- content/.vuepress/config.ts
+++ content/.vuepress/config.ts
@@ -1,8 +1,34 @@
 import { defineUserConfig } from 'vuepress-vite'
+import { hopeTheme } from 'vuepress-theme-hope';
 
 export default defineUserConfig({
-  lang: 'en-US',
-  title: 'Hello VuePress',
-  description: 'Just playing around',
-  base: '/Risus-RPG/'
+  base: '/risus-rpg/',
+  locales: {
+    "/": {
+      lang: 'en-US',
+      title: 'Risus RPG',
+      description: 'Just playing around',
+    },
+    "/th/": {
+      lang: "th-TH",
+      description: 'Just playing around',
+    },
+  },
+
+  theme: hopeTheme({
+  darkmode: 'toggle',
+
+    locales: {
+      "/": {
+        logo: "/logo.png",
+      },
+      "/th/": {
+        logo: "/logo.png",
+        navbarLocales: {
+          selectLangAriaLabel: 'ไทย',
+          langName: 'ไทย'
+        }
+      },
+    },
+  }),
 })
